46559e4b27f7269f2c7806d77228aea0b0284e19
M VIGNESH <134477226+vignesh1234-vi@users.noreply.github.com>
2026-09-03 18:40:10 +0530

    Update SQL insert statement for applicationtable

--- lambda_function.py
+++ lambda_function.py
@@ -47,7 +47,7 @@ def page_router(httpmethod, querystring, formbody):
 def insert_record(formbody):
     formbody = formbody.replace("=", "' : '")
     formbody = formbody.replace("&", "', '")
-    formbody = "INSERT INTO avinashtable value {'" + formbody + "'}"
+    formbody = "INSERT INTO applicationtable value {'" + formbody + "'}"
 
     client = boto3.client('dynamodb')
     response = client.execute_statement(Statement=formbody)
